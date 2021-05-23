58a6a3e459522e3479adf6f0aba37340195a74f0
Hamza-Mughales <hamzawemughales@gmail.com>
2021-05-23 22:45:03 +0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-3 p-5 ">
+            <img src="/img/freeCode.png" alt="" class="rounded-circle " width="150" height="150">
+        </div>
+        <div class="col-md-9 pt-5">
+            <h1>freeCodeCamp</h1>
+            <div class="d-flex">
+                <span class="pr-5"><strong>153</strong> posts</span>
+                <span class="pr-5"><strong>23k</strong> folowers</span>
+                <span class="pr-5"><strong>222</strong> folowing</span>
+            </div>
+            <h4 class="pt-4 font-weight-bold">freecodecamp.org</h4>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, aliquam culpa officia voluptatum eos obcaecati, quod debitis, adipisci optio explicabo atque ipsam beatae consequatur.</div>
+            <a href="#">www.freecodcamp.org</a>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-md-4 mb-4">
+            <img src="/img/slide 1.jpg" class="w-100">
+        </div>
+        <div class="col-md-4 mb-4">
+            <img src="/img/slide 2.jpg" class="w-100">
+        </div>
+        <div class="col-md-4 mb-4">
+            <img src="/img/slide 3.jpg" class="w-100">
+        </div>
+        <div class="col-md-4 mb-4">
+            <img src="/img/slide 4.jpg" class="w-100">
+        </div>
+        <div class="col-md-4 mb-4">
+            <img src="/img/slide 6.png" class="w-100">
+        </div>
+        <div class="col-md-4 mb-4">
+            <img src="/img/slide 2.jpg" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
